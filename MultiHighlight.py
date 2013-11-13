00d3278a6fbd6b85547214ad1b8cc5d4131f2b59
phox <foxwhite@gmail.com>
2013-11-13 16:04:56 +0800

    Add auto select word under cursor

--- MultiHighlight.py
+++ MultiHighlight.py
@@ -1,13 +1,17 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, re
 
 SCOPES = ['variable.parameter', 'string', 'support.constant', 'invalid.illegal', 'invalid.deprecated']
 
 class MultiHighlightCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		regions = self.view.sel()
-	
-		if len(regions) > 0 and regions[0].size() > 0:
-			value = self.view.substr(regions[0])
+		regions = self.view.sel()		
+		
+		if len(regions) > 0:
+			if (regions[0].size() > 0) :
+				value = self.view.substr(regions[0])
+			else :
+				value = self.view.substr(self.view.word(regions[0])).strip()
+				
 			HighlightKeys = self.view.settings().get('hkvalues', [])
 			try:
 				i = HighlightKeys.index(value)
@@ -15,8 +19,21 @@ class MultiHighlightCommand(sublime_plugin.TextCommand):
 				i = -1
 			if i == -1:
 				HighlightKeys.append(value)
-				regions = self.view.find_all(value, sublime.LITERAL)
-				self.view.add_regions(value, regions,  SCOPES[HighlightKeys.index(value) % len(SCOPES)] , '', sublime.HIDE_ON_MINIMAP)
+				search = r"([^_0-9a-zA-Z])"+re.escape(value)+r"([^_0-9a-zA-Z])"
+				print(search)
+				#regions = self.view.find_all(search, sublime.LITERAL)
+				begin = 0
+				from_point = begin
+				regions = []
+				while True:
+						tmp_region = self.view.find(search, from_point)
+						if tmp_region:
+								region = self.view.find(re.escape(value), tmp_region.begin())
+								regions.append(region)
+								from_point = region.end()
+						else:
+								break
+				self.view.add_regions(value, regions,  SCOPES[HighlightKeys.index(value) % len(SCOPES)] , '', sublime.DRAW_EMPTY)
 			else :
 				HighlightKeys.remove(value)
 				self.view.erase_regions(value)
